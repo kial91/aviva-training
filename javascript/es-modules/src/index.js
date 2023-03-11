2eb0cefd5d094f183732afc2fa336a7f53c87211
kial91 <kyle.culbert@and.digital>
2023-03-11 15:09:33 +0000

    feat: Add ES Modules exercise

---
+++ javascript/es-modules/src/index.js
@@ -0,0 +1,10 @@
+//TODO - In order for the program to run we need to import the necessary functions
+
+let myCanvas = create('myCanvas', document.body, 480, 320);
+let reportList = createReportList(myCanvas.id);
+
+let square1 = draw(myCanvas.ctx, 50, 50, 100, 'blue');
+reportArea(square1.length, reportList);
+reportPerimeter(square1.length, reportList);
+
+let square2 = randomSquare(myCanvas.ctx);
