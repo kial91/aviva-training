26707db773a3949d954d611506d080bceda2ec2e
kial91 <kyle.culbert@and.digital>
2023-03-14 15:06:12 +0000

    refactor: Folder name change and exercises/solutions

---
+++ typescript/types/src/index.ts
@@ -0,0 +1,25 @@
+/* TODO - Create a list of cars which have the correct types for the following properties:
+    - manufacturer
+    - model
+    - year
+    - dimensions (object)
+        - height
+        - width
+    - colour
+*/
+let cars = [
+
+];
+cars.forEach((car) => {
+    console.log(`${car.model} - (${car.year})`)
+    console.log(`Manufacturer: ${car.manufacturer}`);
+    console.log(`Colour: ${car.colour}`);
+    console.log(`Size: ${car.dimensions.height * car.dimensions.width}`);
+});
+
+//TODO - Fix the code below so 'value' can be assigned to 'firstName'.
+let value: unknown;
+value = "John";
+
+let firstName: string = value;
+console.log(firstName);
