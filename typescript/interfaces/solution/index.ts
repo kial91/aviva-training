0e0240927f77f34b54a1806978af6d8eed472496
Ashraf Nazar <ashraf.caspian@gmail.com>
2023-03-16 16:15:11 +0000

    fix: correct typos

--- typescript/interfaces/solution/index.ts
+++ typescript/interfaces/solution/index.ts
@@ -1,30 +1,30 @@
 interface IPizza {
-    name: string;
-    slices: number;
-    toppigs: string | Array<string>;
-    price: number;
-    cheescrust?: boolean;
+  name: string;
+  slices: number;
+  toppings: string | Array<string>;
+  price: number;
+  cheeseCrust?: boolean;
 }
 
 let solutionPizza: IPizza[] = [
-    {
-        name: 'Pizza BBQ',
-        slices: 6,
-        toppigs: ['Tomatosauce', 'BBQ sauce'],
-        price: 15,
-        cheescrust: true
-    },
-    {
-        name: 'Margarita',
-        slices: 8,
-        toppigs: 'Tomatosauce and cheese',
-        price: 10
-    },
-    {
-        name: 'Hawaiian',
-        slices: 10,
-        toppigs: 'Ham and pineapple',
-        price: 10,
-        cheescrust: false
-    }
+  {
+    name: 'Pizza BBQ',
+    slices: 6,
+    toppings: ['Tomato sauce', 'BBQ sauce'],
+    price: 15,
+    cheeseCrust: true,
+  },
+  {
+    name: 'Margarita',
+    slices: 8,
+    toppings: 'Tomato sauce and cheese',
+    price: 10,
+  },
+  {
+    name: 'Hawaiian',
+    slices: 10,
+    toppings: 'Ham and pineapple',
+    price: 10,
+    cheeseCrust: false,
+  },
 ];
